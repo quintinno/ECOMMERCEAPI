6fabce334f3f29b0a9847e6135bba2b439a5f19c
Desenvolvimento <josequintinno@outlook.com.br>
2021-08-15 22:58:53 -0300

    ECOMMERCEAPI

---
+++ src/main/resources/carga.basica.sql
@@ -0,0 +1,9 @@
+INSERT INTO TB_CATEGORIA_PRODUTO (CODIGO, DESCRICAO, IMAGEM) VALUES
+(NEXTVAL('hibernate_sequence'), 'Livros', NULL),
+(NEXTVAL('hibernate_sequence'), 'Brinquedos', NULL),
+(NEXTVAL('hibernate_sequence'), 'Casa e Decoração', NULL),
+(NEXTVAL('hibernate_sequence'), 'Casa e Construção', NULL),
+(NEXTVAL('hibernate_sequence'), 'Informática e Acessórios', NULL),
+(NEXTVAL('hibernate_sequence'), 'Saúde e Beleza', NULL),
+(NEXTVAL('hibernate_sequence'), 'Perfumaria', NULL),
+(NEXTVAL('hibernate_sequence'), 'Papelaria', NULL);
